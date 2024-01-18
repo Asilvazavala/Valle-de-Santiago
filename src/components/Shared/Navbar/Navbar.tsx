88d7d27ba635d1979dbc884373dbcb148982d467
Asilvazavala <asilvazavala@gmail.com>
2024-01-18 15:09:44 -0600

    added logo in navbar

--- src/components/Shared/Navbar/Navbar.tsx
+++ src/components/Shared/Navbar/Navbar.tsx
@@ -1,85 +1,123 @@
-import NavbarMobile from '../NavbarMobile/NavbarMobile.tsx';
-import { linksNavbar } from './Navbar-data.astro';
-import { useState } from 'react';
-import Confetti from '../Confetti.tsx'
-import Search from '../../Index/Search/Search.tsx';
+import NavbarMobile from "../NavbarMobile/NavbarMobile.tsx";
+import { linksNavbar } from "./Navbar-data.astro";
+import { useState } from "react";
+import Confetti from "../Confetti.tsx";
+import Search from "../../Index/Search/Search.tsx";
 
 export default function Navbar() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   return (
-    <div id="navbar" className='mb-16 lg:mb-20'>
-      <header className='hidden lg:block z-[9998] bg-gradient-to-r from-primary to-secondary 
-      fixed top-0 w-full'>
-        <div className='py-4 containerSpace flex justify-between items-center relative'>
-          <button 
-            onClick={() => setIsSidebarOpen(!isSidebarOpen)}
-            className={`text-white font-medium hover:text-white/70 
-            transition flex items-center justify-center ${isSidebarOpen && 'opacity-0'}`}>
-            <i className='bx bx-menu-alt-left text-2xl'></i>
-            <span className='text-2xl'>Menú</span>
-          </button>
+    <div id="navbar" className="mb-16 lg:mb-20">
+      <header
+        className="hidden lg:block z-[9998] bg-gradient-to-r from-primary to-secondary 
+      fixed top-0 w-full"
+      >
+        <div className="py-4 containerSpace flex justify-between items-center relative">
+          <aside className="flex gap-2">
+            <a href="/">
+              <img
+                src="/images/general/Logo-Valle.webp"
+                alt="Logo-Valle"
+                title="Logo-Valle"
+                className="h-auto w-24 object-cover"
+              />
+            </a>
+            <button
+              onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+              className={`text-white font-medium hover:text-white/70 
+            transition flex items-center justify-center ${
+              isSidebarOpen && "opacity-0"
+            }`}
+            >
+              <i className="bx bx-menu-alt-left text-2xl"></i>
+              <span className="text-2xl">Menú</span>
+            </button>
+          </aside>
 
-         <Confetti />
-         <Search />
+          <Confetti />
+          <Search />
         </div>
       </header>
 
-      <div className={`shadow-lg fixed z-[9999] top-0 left-0 bg-gradient-to-r from-primary to-secondary 
+      <div
+        className={`shadow-lg fixed z-[9999] top-0 left-0 bg-gradient-to-r from-primary to-secondary 
         flex justify-between items-center h-full duration-700 transition w-68  
-        ${isSidebarOpen 
-          ? 'opacity-100 translate-x-0'
-          : 'opacity-0 translate-x-[-150%]'
-        }`}>
+        ${
+          isSidebarOpen
+            ? "opacity-100 translate-x-0"
+            : "opacity-0 translate-x-[-150%]"
+        }`}
+      >
+        {isSidebarOpen && (
+          <section className="lg:flex hidden relative z-50 overflow-x-hidden">
+            <button
+              onClick={() => setIsSidebarOpen(false)}
+              className="absolute top-0 right-0 text-white hover:text-white/80 transition"
+            >
+              <i className="bx bx-x-circle text-4xl"></i>
+            </button>
 
-          {isSidebarOpen && 
-            <section className="lg:flex hidden relative z-50 overflow-x-hidden">
-              <button 
-                onClick={() => setIsSidebarOpen(false)}
-                className="absolute top-0 right-0 text-white hover:text-white/80 transition">
-                <i className='bx bx-x-circle text-4xl'></i>
-              </button>
+            <nav className="flex flex-col items-start my-24 justify-between h-full">
+              {linksNavbar.map((link, index) => (
+                <ul key={index} className="text-lg text-white font-semibold">
+                  {link.href !== "" ? (
+                    <a href={link.href}>
+                      <li
+                        className="flex items-center justify-center gap-x-2 py-3
+                              border-transparent border-b-[3px] lg:hover:border-b-white transition-colors"
+                      >
+                        <span
+                          className="flex text-xl"
+                          dangerouslySetInnerHTML={{ __html: link.image }}
+                        ></span>
+                        {link.title}
+                      </li>
+                    </a>
+                  ) : (
+                    <li
+                      className="flex items-center justify-center gap-x-2 py-3 group relative w-fit
+                            border-transparent border-b-[3px] lg:hover:border-b-white transition-colors"
+                    >
+                      <span
+                        className="flex text-xl"
+                        dangerouslySetInnerHTML={{ __html: link.image }}
+                      ></span>
+                      {link.title}
+                      <span
+                        className="flex"
+                        dangerouslySetInnerHTML={{ __html: link.icon }}
+                      ></span>
 
-              <nav className="flex flex-col items-start my-24 justify-between h-full">
-                {linksNavbar.map((link, index) => (
-                  <ul key={index} className="text-lg text-white font-semibold">
-                      {link.href !== ''
-                        ? <a href={link.href}>
-                            <li className="flex items-center justify-center gap-x-2 py-3
-                              border-transparent border-b-[3px] lg:hover:border-b-white transition-colors">
-                              <span className="flex text-xl" dangerouslySetInnerHTML={{ __html: link.image }}></span>
-                              {link.title}
-                            </li>
-                          </a>
-                        : <li className="flex items-center justify-center gap-x-2 py-3 group relative w-fit
-                            border-transparent border-b-[3px] lg:hover:border-b-white transition-colors">
-                            <span className="flex text-xl" dangerouslySetInnerHTML={{ __html: link.image }}></span>
-                            {link.title}
-                            <span className="flex" dangerouslySetInnerHTML={{ __html: link.icon }}></span>
-                            
-                            {link.submenu.length > 0 && (
-                              <ul className="group-hover:block absolute hidden top-12 bg-primary 
-                              shadow-md z-50 text-white/70 mt-2">
-                                {link.submenu.map((item, index) => (
-                                  <a key={index} href={item.link}>
-                                    <li className={`px-4 py-2 hover:bg-secondary lg:w-40",
-                                      ${index !== link.submenu.length - 1 && "border-b border-white/20"}`}
-                                    >
-                                      {item.label}
-                                    </li>
-                                  </a>
-                                ))}
-                              </ul>
-                            )}
-                          </li>
-                      }
-                    </ul>
-                  ))}
-              </nav>
-            </section>
-          }
-        </div>
+                      {link.submenu.length > 0 && (
+                        <ul
+                          className="group-hover:block absolute hidden top-12 bg-primary 
+                              shadow-md z-50 text-white/70 mt-2"
+                        >
+                          {link.submenu.map((item, index) => (
+                            <a key={index} href={item.link}>
+                              <li
+                                className={`px-4 py-2 hover:bg-secondary lg:w-40",
+                                      ${
+                                        index !== link.submenu.length - 1 &&
+                                        "border-b border-white/20"
+                                      }`}
+                              >
+                                {item.label}
+                              </li>
+                            </a>
+                          ))}
+                        </ul>
+                      )}
+                    </li>
+                  )}
+                </ul>
+              ))}
+            </nav>
+          </section>
+        )}
+      </div>
       <NavbarMobile />
     </div>
-  )
+  );
 }
--- src/components/Shared/NavbarMobile/NavbarMobile.tsx
+++ src/components/Shared/NavbarMobile/NavbarMobile.tsx
@@ -1,11 +1,13 @@
 import Search from "../../Index/Search/Search.tsx";
 import Confetti from "../Confetti.tsx";
-import { linksNavbar } from '../Navbar/Navbar-data.astro';
-import { useState } from 'react';
+import { linksNavbar } from "../Navbar/Navbar-data.astro";
+import { useState } from "react";
 
 export default function NavbarMobile() {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);  
-  const [submenuStates, setSubmenuStates] = useState(linksNavbar.map(() => false));
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [submenuStates, setSubmenuStates] = useState(
+    linksNavbar.map(() => false)
+  );
 
   const toggleSubmenu = (index: number) => {
     const newSubmenuStates = [...submenuStates];
@@ -14,80 +16,118 @@ export default function NavbarMobile() {
   };
 
   return (
-    <header className="lg:hidden bg-gradient-to-r from-primary to-secondary py-3 md:px-6 fixed 
-    top-0 z-[9998] w-full">
-      <section className="flex justify-between gap-3 items-center">
-        <Confetti />
+    <header
+      className="lg:hidden bg-gradient-to-r from-primary to-secondary py-3 md:px-6 fixed 
+    top-0 z-[9998] w-full"
+    >
+      <section className="flex justify-between gap-4 items-center">
+        <div className="flex gap-2">
+          <a href="/">
+            <img
+              src="/images/general/Logo-Valle.webp"
+              alt="Logo-Valle"
+              title="Logo-Valle"
+              className="h-auto w-20 object-cover"
+            />
+          </a>
+          <Confetti />
+        </div>
 
         <aside className="flex gap-x-2">
-          <div className={`${isMenuOpen && 'hidden'} flex`}>
+          <div className={`${isMenuOpen && "hidden"} flex`}>
             <Search />
           </div>
 
           <i
-            onClick={() => setIsMenuOpen(!isMenuOpen)} 
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
             className={`bx bx-menu-alt-right text-4xl text-white z-50 transition duration-700
-          ${isMenuOpen ? "delay-0 scale-0" : "delay-200"}`}>
-          </i>
+          ${isMenuOpen ? "delay-0 scale-0" : "delay-200"}`}
+          ></i>
 
           <i
-            onClick={() => setIsMenuOpen(!isMenuOpen)}  
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
             className={`bx bx-x scale-0 text-4xl text-white z-50 fixed top-[14px] right-4 transition 
-            duration-700 ${isMenuOpen ?"scale-100 block delay-200" : "delay-0"}`}>
-          </i>
+            duration-700 ${
+              isMenuOpen ? "scale-100 block delay-200" : "delay-0"
+            }`}
+          ></i>
         </aside>
-        
-        <ul className={`z-40 w-full h-screen fixed right-0 top-0 transition duration-700 
+
+        <ul
+          className={`z-40 w-full h-screen fixed right-0 top-0 transition duration-700 
         pt-20 flex flex-col gap-y-8 bg-gradient-to-r from-primary to-secondary overflow-y-auto
-        ${isMenuOpen ? "translate-x-0" : "translate-x-[-150%]"}`}>
+        ${isMenuOpen ? "translate-x-0" : "translate-x-[-150%]"}`}
+        >
           {linksNavbar.map((item, index) => (
             <div key={index}>
-              {item.href !== ''
-                ? <li className="flex w-full text-2xl py-2
-                  border-b border-white px-8">
-                    <a href={item.href} className="w-full text-white flex justify-start items-center gap-x-2">
-                      <span className="text-white flex" dangerouslySetInnerHTML={{ __html: item.image }}></span>
-                      {item.title}
-                    </a>
-                  </li>
-                : <li className="flex flex-col justify-between w-full text-2xl py-2 border-white border-b 
-                  px-8 text-white">
-                    <article 
-                      id={`toggleSubmenu${index}`}
-                      className="flex justify-between items-center"
-                      onClick={() => toggleSubmenu(index)}
-                    >
-                      <div className="flex justify-start items-center gap-x-2">
-                        <span className="text-white flex" dangerouslySetInnerHTML={{ __html: item.image }}></span>
-                        <span>{item.title}</span>
-                      </div>
-                      <span className="text-white flex" dangerouslySetInnerHTML={{ __html: item.icon }}></span>
+              {item.href !== "" ? (
+                <li
+                  className="flex w-full text-2xl py-2
+                  border-b border-white px-8"
+                >
+                  <a
+                    href={item.href}
+                    className="w-full text-white flex justify-start items-center gap-x-2"
+                  >
+                    <span
+                      className="text-white flex"
+                      dangerouslySetInnerHTML={{ __html: item.image }}
+                    ></span>
+                    {item.title}
+                  </a>
+                </li>
+              ) : (
+                <li
+                  className="flex flex-col justify-between w-full text-2xl py-2 border-white border-b 
+                  px-8 text-white"
+                >
+                  <article
+                    id={`toggleSubmenu${index}`}
+                    className="flex justify-between items-center"
+                    onClick={() => toggleSubmenu(index)}
+                  >
+                    <div className="flex justify-start items-center gap-x-2">
+                      <span
+                        className="text-white flex"
+                        dangerouslySetInnerHTML={{ __html: item.image }}
+                      ></span>
+                      <span>{item.title}</span>
+                    </div>
+                    <span
+                      className="text-white flex"
+                      dangerouslySetInnerHTML={{ __html: item.icon }}
+                    ></span>
+                  </article>
 
-                    </article>
-
-                    {item.submenu.length > 0 && (
-                      <ul 
-                        id={`ulMobile${index}`} 
-                        className={`flex-col ${submenuStates[index] ? "" : "hidden"} 
+                  {item.submenu.length > 0 && (
+                    <ul
+                      id={`ulMobile${index}`}
+                      className={`flex-col ${
+                        submenuStates[index] ? "" : "hidden"
+                      } 
                       bg-primary shadow-md text-white z-50`}
-                      >
-                        {item.submenu.map((link, index) => (
-                          <a href={link.link} key={index}>
-                            <li className={`px-4 py-2 hover:bg-secondary lg:w-40 text-white/70                       
-                            ${index !== item.submenu.length - 1 && "border-b border-white/20"}`}                          
-                            >
-                              {link.label}
-                            </li>
-                          </a>
-                        ))}
-                      </ul>
-                    )}
-                  </li>
-              }
-            </div>  
+                    >
+                      {item.submenu.map((link, index) => (
+                        <a href={link.link} key={index}>
+                          <li
+                            className={`px-4 py-2 hover:bg-secondary lg:w-40 text-white/70                       
+                            ${
+                              index !== item.submenu.length - 1 &&
+                              "border-b border-white/20"
+                            }`}
+                          >
+                            {link.label}
+                          </li>
+                        </a>
+                      ))}
+                    </ul>
+                  )}
+                </li>
+              )}
+            </div>
           ))}
         </ul>
       </section>
     </header>
-  )
+  );
 }
